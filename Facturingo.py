1c7f72a321bc1a42bdcd9bc1b25849b9485c4c7b
Landry Simo <landrysimo99@gmail.com>
2024-05-30 14:58:56 +0100

    add style

--- Facturingo.py
+++ Facturingo.py
@@ -4,8 +4,9 @@ from PyQt5.QtWidgets import QApplication
 
 from executers import windowHome
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
+    # app.setStyleSheet(open("styles/futurist.qss", "r").read())
     home = windowHome.Home()
     home.show()
     app.exec_()
